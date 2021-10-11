824857cc5b3a98493e8567c68c4158e1b976d53c
Jeremy Boetticher <me4jeremyboett@gmail.com>
2021-10-10 23:36:42 -0500

    db

--- skeleton_parser_v2.py
+++ skeleton_parser_v2.py
@@ -79,13 +79,13 @@ Transform a quotation in the description attribute to have quotations around it
 
 def quotation_editor(description):
     initial_length = len(description)
-    new_description = ""
+    new_description = "\""
     for i in range(initial_length):
         if description[i] == "\"":
-            new_description += "\"" + description[i] + "\""
+            new_description += "\"" + description[i]
         else:
             new_description += description[i]
-    return new_description
+    return new_description + "\""
 
 
 """
@@ -105,35 +105,33 @@ def parseJson(json_file):
             #Makes Items table
             for col_name in Items_columns:
                 if col_name == "Started" or col_name == "Ends":
-                    if col_name not in items[i] or items[i][col_name] == Non:
+                    if col_name not in items[i] or items[i][col_name] == None:
                         Items_string += "NULL" + columnSeparator
                     else:
-                        Items_string += str(transformDttm(items[i][col_name])) + columnSeparator
+                        Items_string += str(quotation_editor(transformDttm(items[i][col_name]))) + columnSeparator
                 elif col_name == "Currently" or col_name == "Buy_Price":
                     if col_name not in items[i] or items[i][col_name] == None:
-
-                        Items_string += "NULL" + columnSeparator
-                    else:
-                        Items_string += str(transformDollar(items[i][col_name]))
-                elif col_name == "First_Bid":
-                    if col_name not in items[i] or items[i][col_name] == None:
-
                         Items_string += "NULL" + columnSeparator
                     else:
                         Items_string += str(transformDollar(items[i][col_name])) + columnSeparator
-                elif col_name == "Description":
+                elif col_name == "First_Bid":
+                    if col_name not in items[i] or items[i][col_name] == None:
+                        Items_string += "NULL" + columnSeparator
+                    else:
+                        Items_string += str(transformDollar(items[i][col_name])) + columnSeparator
+                elif col_name == "Description" or col_name == "Name" or col_name == "Country" or col_name == "Location":
                     if col_name not in items[i] or items[i][col_name] == None:
 
-                        Items_string += "NULL"
+                        Items_string += "NULL" + columnSeparator
                     else:
-                        Items_string == str(quotation_editor(items[i][col_name]))
+                        Items_string += str(quotation_editor(items[i][col_name])) + columnSeparator
                 else:
                     if col_name not in items[i] or items[i][col_name] == None:
 
                         Items_string += "NULL" + columnSeparator
                     else:
                         Items_string += str(items[i][col_name]) + columnSeparator
-            Items_string += '\n'
+            Items_string += "\"" + str(items[i]["Seller"]["UserID"]) + "\"" + '\n'
 
             #Makes User table and Bids_On table
             if items[i]["Bids"] != None:
